9631d3b28f2d1eacbccfd5cdfbd67fce609fdea4
Rémy <remyc59440@hotmail.com>
2022-11-22 10:03:16 +0100

    exo 71 ok

--- exercice1.js
+++ exercice1.js
@@ -1,11 +1,13 @@
-let monDiv = .createElement('div');
+let myDelete = document.getElementById("aSupprimer").style.display = 'none';
 
+let monDiv = document.createElement('div');
 document.getElementById('viewport').appendChild(monDiv);
 
+let monDivTwo = document.createElement('div');
+document.getElementById('viewport').appendChild(monDivTwo);
 
-
-
-
-
-
+monDiv.innerHTML = "Ce div a été ajouté en utilisant javascript"
+monDivTwo.style.backgroundColor = "red";
+monDivTwo.style.width = "100px";
+monDivTwo.style.height = "100px";
 
